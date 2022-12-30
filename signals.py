a475ab7bff0b79f81fb3842776b42fb1c96e72c5
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2022-12-30 20:21:09 +0300

    Add files via upload

--- signals.py
+++ signals.py
@@ -10,11 +10,13 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.H2('Fourier Transformation'),
     html.P("samples"),
-    dcc.Input(id='num', type='number', min=2, max=1024, step=2, value=64),
-    html.P("Amplitudes (sep='  ')"),
-    dcc.Input(type='text', value='1 1 1', id='amplitudes', debounce=True),
+    dcc.Input(id='num', type='number', min=2, max=1024, step=2, value=64, debounce=True),
+    html.P("Bias + Amplitudes (sep='  ')"),
+    dcc.Input(type='text', value='1 1 1 1', id='amplitudes', debounce=True),
+    html.P("Phases (sep='  ')"),
+    dcc.Input(type='text', value='1.57 0 0.42', id='phases', debounce=True),
     dcc.Graph(id="graph"),
-    dcc.Slider(id='max_hz', min=0, max=32, value=0, step=1),
+    dcc.Slider(id='max_hz', min=0, max=24, value=0, step=1),
 ])
 
 
@@ -22,12 +24,15 @@ app.layout = html.Div([
     Output("graph", "figure"),
     Input("max_hz", "value"),
     Input("num", "value"),
-    Input("amplitudes", "value")
+    Input("amplitudes", "value"),
+    Input("phases", "value")
 )
-def plot_sin(i, n_samples, amps):
+def plot_sin(i, n_samples, amps, phases):
     amps = list(map(float, amps.strip().split(' ')))
-    x = np.pi * np.linspace(0, 2, n_samples)
-    signals = [a * np.sin(x * (f + 1)) for f, a in enumerate(amps[1:])]
+    phases = list(map(float, phases.strip().split(' ')))
+    phases.extend([0] * (len(amps) - len(phases)))
+    x = np.pi * np.linspace(0, 2, n_samples, endpoint=False)
+    signals = [a * np.sin(x * (f + 1) + phi) for f, (a, phi) in enumerate(zip(amps[1:], phases))]
     y = np.array(signals).sum(axis=0) + amps[0]
     i += 1
     fig = make_subplots(rows=1, cols=3, column_widths=[0.5, 0.25, 0.25])
@@ -46,11 +51,19 @@ def plot_sin(i, n_samples, amps):
     amplitudes = 2 * np.abs(rft) / len(x)
     amplitudes[0] /= 2
     frequencies = np.fft.fftfreq(len(x)) * len(x)
+    phases = np.arctan2(rft.imag, rft.real)
+    phases += np.pi / 2
+    phases[amplitudes < 0.1] = 0
+    phases[0] = 0
     fig.add_trace(go.Scatter(x=frequencies[:len(frequencies) // 2],
                              y=amplitudes[:len(fft) // 2], name='Amplitude'), col=3, row=1)
+    fig.add_trace(go.Scatter(x=frequencies[:len(frequencies) // 2],
+                             y=phases[:len(fft) // 2], name='Phase', line=go.scatter.Line(dash='dot')), col=3, row=1)
 
     for n, signal in enumerate(signals):
         fig.add_trace(go.Scatter(x=x, y=signal + amps[0], opacity=0.3, name=f'signal {n + 1}'))
+
+    fig.update_xaxes(range=[0, 32], col=3)
     fig.update_yaxes(range=[y.min() * 1.2, y.max() * 1.2], col=1)
     fig.update_xaxes(title_text="Frequency", row=1, col=2)
     fig.update_yaxes(title_text="Amplitude", row=1, col=3)
